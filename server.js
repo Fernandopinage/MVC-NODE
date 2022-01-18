512d869602ea3679ea64a0fe2cb5fa67a59cc06c
Fernando Pinage <luizfernandoluck@hotmail.com>
2022-01-17 23:47:22 -0300

    ok

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 const Router = require('./Router/index')
 const express = require('express');
 const app = express();
-
+const axios = require('axios');
 const Port = process.env.Port || '3000';
 
 app.use('/',Router);
+
+app.get('/pokemon', async(req,res)=>{
+
+    try {
+        const response = await axios.get('https://pokeapi.co/api/v2/pokemon/pikachu');
+        console.log(response);
+      } catch (error) {
+        console.error(error);
+      }
+});
 
 app.listen(Port,()=>{
 
