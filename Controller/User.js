2ea7d4fe1bef217e2a129149741c41502dbade9e
FernandoPinage <luizfernandoluck@hotmail.com>
2022-01-19 15:03:06 -0400

    ok

--- Controller/User.js
+++ Controller/User.js
@@ -4,20 +4,20 @@ class User{
     index(req,res){
 
        //res.render('ok')
-       res.render('../views/index')
+       res.render('../View/index')
 
     }
 
     sobre(req,res){
 
-        res.render('../views/sobre')
+        res.render('../View/sobre')
 
 
     }
 
     create(req,res){
 
-       res.render('../views/create')
+       res.render('../View/create')
     }
 
     insert(req,res){
--- Router/index.js
+++ Router/index.js
@@ -10,9 +10,10 @@ app.set('view engine', 'html');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+/* Rota de Usuario */
 app.get('/', User.index);
 app.get('/sobre',User.sobre);
 app.get('/novo/usuario',User.create);
 app.post('/inser/usuario',User.insert);
-
+/* **************** */
 module.exports = app;
