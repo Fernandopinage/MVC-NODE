a67b9e4db84f9626f0331fd6797cb84036a5cbbb
Fernando Pinage <luizfernandoluck@hotmail.com>
2022-01-20 17:43:15 -0300

    ok

--- Modal/User.js
+++ Modal/User.js
@@ -7,13 +7,16 @@ class ModalUser {
      create(req, res) {
 
         let dados = req.body;
-        let sql = "INSERT INTO `usuario`(`user_id`, `user_nome`, `user_email`, `user_telefone`, `user_senha`, `user_cep`, `user_uf`, `user_endereco`, `user_cidade`, `user_bairro`)  VALUES (null, '" + dados.nome + "', '" + dados.email + "', '" + dados.telefone + "', '" + dados.password + "', '" + dados.cep + "', '" + dados.uf + "', '" + dados.endereco + "', '" + dados.cidae + "', '" + dados.bairro + "')";
+        console.log(dados)
+        let sql = "INSERT INTO `usuario`(`user_id`, `user_nome`, `user_email`, `user_telefone`, `user_senha`, `user_cep`, `user_uf`, `user_endereco`, `user_cidade`, `user_bairro`)  VALUES (null, '" + dados.nome + "', '" + dados.email + "', '" + dados.telefone + "', '" + dados.password + "', '" + dados.cep + "', '" + dados.uf + "', '" + dados.endereco + "', '" + dados.cidade + "', '" + dados.bairro + "')";
 
          con.query(sql, (err) => {
 
             if (!err) {
 
-                
+                console.log('sucess')
+            }else{
+                console.log(err)
             }
 
         })
